49c59495e8b199d9784bb65e5c26d52f5e48b134
vmaggioli <vinmags16@gmail.com>
2020-12-15 10:21:35 -0500

    Day 15 part 1

---
+++ 15/index.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+function numberGame() {
+  const startingNumbers = fs.readFileSync("input.txt", "utf8").split(",");
+  const end = 2020;
+  var lastNumberSpoken;
+  var turn = 0;
+  const numberMap = {};
+
+  // Starting numbers
+  for (turn; turn < startingNumbers.length; turn++) {
+    numberMap[startingNumbers[turn]] = [turn + 1];
+  }
+  turn++;
+  lastNumberSpoken = parseInt(startingNumbers[startingNumbers.length - 1]);
+
+  while (turn != end + 1) {
+    if (
+      numberMap[lastNumberSpoken].length == 1 &&
+      numberMap[lastNumberSpoken][0] == turn - 1
+    ) {
+      if (numberMap[0] == undefined) numberMap[0] = [];
+      numberMap[0].push(turn);
+      lastNumberSpoken = 0;
+      turn++;
+      continue;
+    }
+
+    const turns = numberMap[lastNumberSpoken];
+    const currentNumber = turns[turns.length - 1] - turns[turns.length - 2];
+    if (numberMap[currentNumber] == undefined) numberMap[currentNumber] = [];
+    numberMap[currentNumber].push(turn);
+    lastNumberSpoken = currentNumber;
+    turn++;
+  }
+
+  //   console.log(numberMap)
+  return lastNumberSpoken;
+}
+
+console.log(numberGame());
